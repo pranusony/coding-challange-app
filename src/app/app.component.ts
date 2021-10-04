86aee2da6b0ea22ceb51805d122aa94e33faec50
Pranusha <pranu.kasireddy@gmail.com>
2021-10-03 22:30:40 -0600

    angular adding grid and ability to search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable, Subject} from "rxjs";
+import {debounceTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'coding-challange-app';
+
+  displayedColumns: string[] = ['name', 'specialty'];
+  results$:Observable<any> = new Observable<any>();
+  search$ = new Subject<string>();
+  searchField:string = 'name';
+  ngModelOptions = {
+    standalone:true
+  }
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.loadData();
+     this.search$.pipe(debounceTime(300)).subscribe((value) => {
+        this.loadData({
+          search:value,
+          field:this.searchField
+        });
+     })
+  }
+
+  loadData(filter?:{
+    search:string,
+    field:string
+  }) {
+    if(filter) {
+      this.results$ = this.http.get(`http://localhost:3000/api/data?search=${filter.search}&field=${filter.field}`)
+    } else {
+      this.results$ = this.http.get("http://localhost:3000/api/data");
+    }
+  }
+
+  handleSearch(event:any) {
+    this.search$.next(event.target.value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MaterialModule} from './material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
